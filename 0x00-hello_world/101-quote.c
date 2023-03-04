abbb3ea30e5f42865fcea0b834f250616dce834b
younqwharlork <opeoluwaajayi05@gmail.com>
2023-03-04 10:56:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A c program that prints a line to the standard error
+ * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
